39ef2183849308a6beab587214b681351c165aa1
Deniz Atakan <Deniz Atakan>
2016-03-30 15:19:37 +1100

    Running app which has been debugged

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,27 @@
-var myApp= angular.module('myApp',[]) //links js and html together
+var myApp = angular.module('myApp', []) //links js and html together
 
-myApp.controller('MyContoller', function ($scope) {
+myApp.service('HistoryService', function($http){
+	var BaseUrl= "http://localhost:8080/"
+
+	this.saveWord = function(newWord) {
+		var url = BaseUrl +"saveCurrent"
+		return $http.post(url, {word: newWord})
+	}//this function is a service which retrieves the value from the HTML file and sends to tehe server
+
+})// creates a memory service to recall previously submitted words. $http because interfaces with the internet not with the host
+
+myApp.controller('MyController', function ($scope, HistoryService) {
 	$scope.newWord ='cat'
+	$scope.saveThisWord= function(){
+		HistoryService.saveWord( $scope.newWord )//returns a 'promise'; a future value to be filled
+		.then(saveSuccess, error)
+	}//hooks up the button to the index.html meaning it takes the input properly.
+
+	function saveSuccess(json){
+		console.log(json)
+	}
+
+	function error(json){
+			console.log(err)
+	}
 }) //scope defines the array of data the html file has access to
--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,24 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-Parser')
+var Nedb =require('nedb')
+
+var database = new Nedb({filename: '.data/data.db' , autoload: true})
 
 app.use(express.static('../Client'))
 
+app.use(bodyParser.json())
+
+app.post('/saveCurrent', function (req, res) {
+	var data = {
+		word: req.body.word, 
+		date: Date.now()}
+
+	var done= function () {
+	console.log("Got a request to save something!")
+	res.end("done")	
+	}
+
+database.insert(data, done)	
+}) //req=info set by web app from client, res=response, i.e. what is sent back use res.send to send something in response
 app.listen(8080)
