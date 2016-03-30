f500fc46b0cf24deacadcdea1ba1427bd23b86a2
Deniz Atakan <Deniz Atakan>
2016-03-30 15:39:49 +1100

    working server

--- Client/index.js
+++ Client/index.js
@@ -8,20 +8,35 @@ myApp.service('HistoryService', function($http){
 		return $http.post(url, {word: newWord})
 	}//this function is a service which retrieves the value from the HTML file and sends to tehe server
 
+	this.getSaved = function(newWord) {
+			var url = BaseUrl +"getSaved"
+			return $http.get(url)
+	}
+
 })// creates a memory service to recall previously submitted words. $http because interfaces with the internet not with the host
 
 myApp.controller('MyController', function ($scope, HistoryService) {
 	$scope.newWord ='cat'
+	$scope.word= []
 	$scope.saveThisWord= function(){
 		HistoryService.saveWord( $scope.newWord )//returns a 'promise'; a future value to be filled
 		.then(saveSuccess, error)
 	}//hooks up the button to the index.html meaning it takes the input properly.
+
+	$scope.function getSavedWords(){
+		HistoryService.getsaved()
+		.then(loadSuccess, error)
+	}
 
 	function saveSuccess(json){
 		console.log(json)
 	}
 
 	function error(json){
-			console.log(err)
+		console.log(err)
+	}
+
+	function loadSuccess(json){
+		$scope.words=json.data
 	}
 }) //scope defines the array of data the html file has access to
--- Server/Server.js
+++ Server/Server.js
@@ -21,4 +21,18 @@ app.post('/saveCurrent', function (req, res) {
 
 database.insert(data, done)	
 }) //req=info set by web app from client, res=response, i.e. what is sent back use res.send to send something in response
+
+app.get('/getSaved', function (req,res) {
+
+	//query
+	var query = {} //because empty braces grabs everything because searches everything
+
+	var done= function(err, data) {
+		console.log("data read from database")
+		res.send(data)
+	}
+	
+	database.find(query, done)
+})
+
 app.listen(8080)
